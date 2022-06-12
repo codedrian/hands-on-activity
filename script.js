5824e29dc65ee4737d00cccb368bf1f44d48d32d
Adrian Gaile Singh <adriangailee@gmail.com>
2022-06-12 22:20:03 +0800

    add function to show definition

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function showXml() {
-  document.getElementById("first").innerHTML = "ASAS";
+  document.getElementById('first').style.display = "block";
+}
+function showUsage() {
+  document.getElementById('second').style.display = "block";
+}
+function showMarkUp() {
+  document.getElementById('third').style.display = "block";
+}
+
+function showXMLPL() {
+  document.getElementById('fourth').style.display = "block";
 }
 function showSyntax() {
-  document.getElementById("second").innerHTML = "hahaha";
+  document.getElementById('fifth').style.display = "block";
+}
+ function syntaxRules() {
+  document.getElementById('sixth').style.display = "block";
+}
+function xmlDeclaration() {
+  document.getElementById('seventh').style.display = "block";
+}
+function showSyntaxRules() {
+  document.getElementById('eighth').style.display = "block";
+}
+function showTagsElements() {
+  document.getElementById('nineth').style.display = "block";
+}
+function showXMLAtrributes() {
+  document.getElementById('tenth').style.display = "block";
 }
